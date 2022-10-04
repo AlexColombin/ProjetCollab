551cb66c0f26e8a3253ac5ba4516bdb949484c7e
AlexColombin <115042819+AlexColombin@users.noreply.github.com>
2022-10-05 01:06:14 +0200

    Update Main.java

--- src/fr/sabri/test/Main.java
+++ src/fr/sabri/test/Main.java
@@ -8,7 +8,7 @@ public class Main {
 		Ville v1=new Ville();
 		Ville v2=new Ville("Rafraf","Tunisie",70000);
 		Ville v3=new ville();
-		Ville v4=new ville();
+		Ville v4=new ville("paris","france",73000);
 		System.out.println(v1);
 		System.out.println(v2);
 	
