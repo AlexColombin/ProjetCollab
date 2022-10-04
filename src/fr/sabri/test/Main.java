d102bedaf9a4c1f2b7ff69c776686aa5d32de45b
Sabri Ben Raies <sabri.benrais@gmail.com>
2022-10-05 01:18:28 +0200

    conflict cv

--- src/fr/sabri/test/Main.java
+++ src/fr/sabri/test/Main.java
@@ -8,11 +8,8 @@ public class Main {
 		Ville v1=new Ville();
 		Ville v2=new Ville("Rafraf","Tunisie",70000);
 		Ville v3=new ville();
-<<<<<<< HEAD
 		Ville v4=new ville("ras jebel","tunisie",700);
-=======
-		Ville v4=new ville("paris","france",73000);
->>>>>>> 551cb66c0f26e8a3253ac5ba4516bdb949484c7e
+		Ville v5=new ville("paris","france",73000);
 		System.out.println(v1);
 		System.out.println(v2);
 	
