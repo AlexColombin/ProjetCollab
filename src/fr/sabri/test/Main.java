7752c88f97e1e964d27a614b8d4c0e813fbdb944
Sabri Ben Raies <sabri.benrais@gmail.com>
2022-10-05 00:04:26 +0200

    ajout autre vilel

--- src/fr/sabri/test/Main.java
+++ src/fr/sabri/test/Main.java
@@ -7,6 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		Ville v1=new Ville();
 		Ville v2=new Ville("Rafraf","Tunisie",70000);
+		Ville v3=new ville();
 		System.out.println(v1);
 		System.out.println(v2);
 	
