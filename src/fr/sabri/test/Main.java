87e4b9975954b9394b5ccdd6d0746c118a6d2681
Sabri Ben Raies <sabri.benrais@gmail.com>
2022-11-08 13:51:06 +0100

    effacer tout

--- src/fr/sabri/test/Main.java
+++ src/fr/sabri/test/Main.java
@@ -6,12 +6,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Ville v3=new Ville();
-		Ville v4=new ville("bizerte","tunisie",700);
-		Ville v5=new ville("Nancy","france",73000);
-		Ville v6=new ville("marseille","france",73000);
-		Ville v8=new Ville();
-		Ville v9=new Ville();
+		Ville v1=new Ville();
+		
 
 		System.out.println(v1);
 		System.out.println(v2);
