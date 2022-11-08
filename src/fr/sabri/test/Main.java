228833e681501fa6c3a9b6e2461db31c4f3289bd
Sabri Ben Raies <sabri.benrais@gmail.com>
2022-11-08 13:15:31 +0100

    skjdk

--- src/fr/sabri/test/Main.java
+++ src/fr/sabri/test/Main.java
@@ -9,9 +9,6 @@ public class Main {
 		Ville v4=new ville("bizerte","tunisie",700);
 		Ville v5=new ville("Nancy","france",73000);
 		Ville v6=new ville("marseille","france",73000);
-		Ville v7=new ville();
-		
-		Ville v8=new Ville();
 		Ville v9=new Ville();
 
 		System.out.println(v1);
