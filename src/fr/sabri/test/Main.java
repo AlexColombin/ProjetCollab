ad3100cb7030479e5d432258473b70007dcd1771
Sabri Ben Raies <sabri.benrais@gmail.com>
2022-11-08 13:18:32 +0100

    Merge branch 'savri'

--- src/fr/sabri/test/Main.java
+++ src/fr/sabri/test/Main.java
@@ -8,12 +8,9 @@ public class Main {
 		Ville v1=new Ville();
 		Ville v4=new ville("bizerte","tunisie",700);
 		Ville v5=new ville("Nancy","france",73000);
-<<<<<<< HEAD
 		Ville v6=new ville("marseille","france",73000);
-=======
 		
 		Ville v8=new Ville();
->>>>>>> savri
 		Ville v9=new Ville();
 
 		System.out.println(v1);
