62c734e8b6cb9e8204bd8da650ce5db62d6cff65
Sabri Ben Raies <sabri.benrais@gmail.com>
2022-11-08 13:25:02 +0100

    effacer

--- src/fr/sabri/test/Main.java
+++ src/fr/sabri/test/Main.java
@@ -8,9 +8,6 @@ public class Main {
 		Ville v1=new Ville();
 		Ville v2=new Ville();
 		Ville v3=new Ville();
-		Ville v4=new ville("bizerte","tunisie",700);
-		Ville v5=new ville("Nancy","france",73000);
-		Ville v6=new ville("marseille","france",73000);
 		Ville v7=new Ville();
 		Ville v8=new Ville();
 		Ville v9=new Ville();
